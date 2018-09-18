67d2929182971cbedbe710d6e89f8146331565da
gene-radcliffe <g_radcliffe@hotmail.com>
2018-09-18 12:48:10 -0400

    removed aws key

--- config/initializers/carrierwave.rb.rb
+++ config/initializers/carrierwave.rb.rb
@@ -2,8 +2,7 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       :provider               => 'AWS',       # required
-      :aws_access_key_id      => 'AKIAI4FBVO6MR5QNXKKA',       # required
-      :aws_secret_access_key  => '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',       # required
+           # required
       :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
     }
     config.fog_directory  = 'gradcliffe82' # required
